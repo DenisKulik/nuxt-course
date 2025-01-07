a2bcd0a52737a8b781ddd811cad95827a46e5b56
dkulik <kulikdenis1996x@gmail.com>
2025-01-07 15:42:02 +0700

    feature: assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
+    <img src="~/assets/img_1.webp" alt="logo" width="100" />
+    <IconsBell />
     <Alert />
     <Profile />
   </div>
 </template>
-
-<style scoped></style>
